958c3ca9b56fa691fd2aee48d5e85a23fdcb872b
alekseynechunaev <aleksey.nechunaev@gmail.com>
2023-05-01 14:02:15 +0700

    fix test config

--- src/test/java/hexlet/code/config/TestConfiguration.java
+++ src/test/java/hexlet/code/config/TestConfiguration.java
@@ -1,14 +1,17 @@
 package hexlet.code.config;
 
+import com.github.javafaker.Faker;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@Profile(TestConfig.TEST_PROFILE)
 @ComponentScan(basePackages = "hexlet.code")
 @PropertySource(value = "classpath:application.yaml")
-public class TestConfig {
-    public static final String TEST_PROFILE = "test";
+public class TestConfiguration {
+    @Bean
+    public Faker faker() {
+        return new Faker();
+    }
 }
